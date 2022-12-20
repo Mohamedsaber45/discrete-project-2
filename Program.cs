bb428f8fec2ce5d9e82a2b41607c4dc7d874930f
Mohamedsaber45 <120626930+Mohamedsaber45@users.noreply.github.com>
2022-12-20 20:33:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Max");
+            int Max=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            for(int number = int.Parse(Console.ReadLine()); number <= Max; number++)
+            {
+                int sum = 0;
+                for(int i =1; i<=number; i++)
+                {
+                    
+                    if(number % i ==0)
+                    {
+                        sum++;
+                    }
+                } 
+                if(sum == 2)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.ReadKey();  
+        }
+    }
+}
